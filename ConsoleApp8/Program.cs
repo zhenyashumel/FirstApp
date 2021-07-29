671625110a13c85b88df21f264b3e252bacca01b
Eugen.Shumel <eugen.shumel@geeksolutions.co>
2021-07-29 20:55:24 +0300

    Init

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
